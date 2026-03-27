0ea1864c2b48f1f4278dc3783b5079a04ade8f9a
FengZhaolin <kingsimba0511@gmail.com>
2026-03-27 21:21:10 +0800

    add obsolete topics

--- src/objectStore.ts
+++ src/objectStore.ts
@@ -107,10 +107,53 @@ export class ArrayObjectStore<T> {
   }
 }
 
+class DeviceInfoStore extends ObjectStore<DeviceInfo> {
+  private process(info: DeviceInfo | null): DeviceInfo | null {
+    if (!info || !info.robot) return info;
+
+    // Assign default visualization topics based on device model if undefined
+    // WARNING: These topics(containing "matched") are all obsolete. They has been replaced by binary format.
+    if (!info.robot.visualization_topics) {
+      if (info.device?.model?.startsWith('forklift')) {
+        // forklifts.
+        info.robot.visualization_topics = [
+          '/horizontal_laser_2d/matched',
+          '/left_laser_2d/matched',
+          '/right_laser_2d/matched',
+          '/rb_laser_2d/matched',
+          '/matched_depth_points/downward',
+          '/matched_depth_points/backward',
+        ];
+      } else {
+        // others
+        info.robot.visualization_topics = [
+          '/horizontal_laser_2d/matched',
+          '/bottom_laser_2d/matched',
+          '/matched_depth_points/downward',
+          '/matched_depth_points/forward',
+        ];
+      }
+    }
+    return info;
+  }
+
+  setObject(items: DeviceInfo | null): void {
+    super.setObject(this.process(items));
+  }
+
+  async refresh(): Promise<DeviceInfo | null> {
+    const res = await robotApi.get(this.endpoint);
+    const data = await res.json();
+    this.stored_object = this.process(data);
+    this.notify();
+    return this.stored_object;
+  }
+}
+
 export const bagListStore = new ArrayObjectStore<BagItem>('bags/');
 export const mapListStore = new ArrayObjectStore<MapItem>('maps/');
 export const mappingTaskListStore = new ArrayObjectStore<MappingTaskItem>('mappings/');
 export const videoListStore = new ArrayObjectStore<VideoFileItem>('videos/');
 export const alertTriggeredBagStore = new ArrayObjectStore<BagItem>('recording/');
-export const deviceInfoStore = new ObjectStore<DeviceInfo>('device/info');
+export const deviceInfoStore = new DeviceInfoStore('device/info');
 export const coreDumpStore = new ArrayObjectStore<CoreDumpItem>('core_dumps/');
